34fb40ec978a229fa965482b54d2cd65b73cd405
Rodrigo Martins <rodmajc@gmail.com>
2020-05-29 09:46:21 -0300

    Desejando um bom dia

--- GitTeste/Program.cs
+++ GitTeste/Program.cs
@@ -5,8 +5,8 @@ namespace GitTeste {
 		static void Main(string[] args) {
 
 			Console.WriteLine("Olá Mundo!!!");
-			Console.WriteLine("Bom dia");
 			Console.WriteLine("Como você está hoje");
+			Console.WriteLine("Tenha um bom dia!");
 
 			Console.ReadLine();
 		}
